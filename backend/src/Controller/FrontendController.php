a115d7a5f0d2bd71a877fc1e760647afdfd25df2
StarbugStone <pint2stone@gmail.com>
2025-05-25 15:36:42 +0200

    fix: updating routes to serve from symfony backend

---
+++ backend/src/Controller/FrontendController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controller to serve the React frontend application
+ */
+class FrontendController extends AbstractController
+{
+    /**
+     * Serves the React application for any non-API routes
+     * This allows React Router to handle client-side routing
+     */
+    public function index(): Response
+    {
+        // Return the index.html file that loads the React app
+        $indexFile = $this->getParameter('kernel.project_dir') . '/public/index.html';
+        
+        if (!file_exists($indexFile)) {
+            throw $this->createNotFoundException('Frontend application not found');
+        }
+        
+        $content = file_get_contents($indexFile);
+        
+        return new Response($content, Response::HTTP_OK, [
+            'Content-Type' => 'text/html'
+        ]);
+    }
+}
